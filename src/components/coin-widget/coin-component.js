f6ff87209749f603145dea9930827ba19b579038
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-04 17:10:00 -0700

    Add coin widget that displays coin id (name)

---
+++ src/components/coin-widget/coin-component.js
@@ -0,0 +1,10 @@
+function CoinWidget(props) {
+
+  console.log(props)
+
+  return (
+    <h1>{props.coin.id}</h1>
+  )
+}
+
+export default CoinWidget
--- src/components/dashboard/dashboard-component.js
+++ src/components/dashboard/dashboard-component.js
@@ -1,13 +1,16 @@
 import fetchRequests from '../../utilities/fetch-requests'
 import React, { useState, useEffect } from 'react'
+import CoinWidget from '../coin-widget/coin-component'
 
 function Dashboard() {
 
   const [allCoins, setAllCoins] = useState([])
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     fetchRequests.fetchAllCoins()
-    .then(results => setAllCoins(results))  
+    .then(results => setAllCoins(results))
+    .then(setLoading(false))
   }, [])
 
 
@@ -16,7 +19,9 @@ function Dashboard() {
   return (
     <>
       <h1>All Coins
-
+        {!isLoading &&
+          allCoins.map(coin => <CoinWidget coin={coin}/>)
+        }
       </h1>
     </>
   )
