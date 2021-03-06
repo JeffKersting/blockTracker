9b9a9417b4c685851316f0e72649fd5754484a4d
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-05 20:44:41 -0700

    Add 24hr price change with positive/negative styling

--- src/components/coin-widget/coin-component.js
+++ src/components/coin-widget/coin-component.js
@@ -1,22 +1,30 @@
 function CoinWidget({ coin, favorited, addFavorite }) {
 
   let favoriteStatus
+  let priceChange = coin.price_change_24h > 0 ? 'positive' : 'negative'
   favorited === 'favorited-coin' ? favoriteStatus='Unwatch' : favoriteStatus='Watch'
+
   console.log(coin)
   return (
     <div className='widget-container'>
       <h1>{coin.id}
-      <img
-        src={coin.image}
-        alt={coin.id}
-      />
+        <img
+          src={coin.image}
+          alt={coin.id}
+        />
       </h1>
-      <h2>Symbol: {coin.symbol}</h2>
       <h2>
-        {coin.current_price.toLocaleString('en-us', {
+        {`Symbol: ${coin.symbol}`}
+      </h2>
+      <h2>{
+        `Current Price:
+        ${coin.current_price.toLocaleString('en-us', {
           style: 'currency',
           currency: 'USD'
-        })}
+        })}`
+      }</h2>
+      <h2>
+        24 hour change <p className={priceChange}>{coin.price_change_24h}</p>
       </h2>
       <div
         className={`favorite ${favorited}`}
@@ -24,7 +32,6 @@ function CoinWidget({ coin, favorited, addFavorite }) {
         onClick={event => addFavorite(event)}
       >
         {favoriteStatus}
-
       </div>
     </div>
   )
