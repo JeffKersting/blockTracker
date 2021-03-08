b4741263922077d32c25a3fe27cdd9f8d6d17699
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-08 11:26:25 -0700

    Add coinwidget proptype check to prop-type-check file

--- src/utilities/prop-type-check.js
+++ src/utilities/prop-type-check.js
@@ -1,1 +1,21 @@
 import PropTypes from 'prop-types'
+
+//widget proptype test
+CoinWidget.propTypes = {
+  coin: PropTypes.arrayOf(PropTypes.shape(
+    {
+      current_price: PropTypes.number,
+      id: PropTypes.string,
+      image: PropTypes.string,
+      market_cap: PropTypes.number,
+      name: PropTypes.string,
+      price_change_24h: PropTypes.number,
+      price_change_percentage_24h: PropTypes.number,
+      symbol: PropTypes.string,
+      total_volume: PropTypes.number
+    }
+  )),
+  currency: PropTypes.string,
+  favorited: PropTypes.string,
+  addFavorite: PropTypes.func
+}
