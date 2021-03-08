f3eddd315dcc30420c847c6dfd2d7ce3c991fdb7
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-03-08 13:56:52 -0700

    Add error handler to fetch requests utility file

--- src/utilities/fetch-requests.js
+++ src/utilities/fetch-requests.js
@@ -1,13 +1,17 @@
 const fetchRequests = {
-  // fetchErrorHandler: (response) => {
-  //   if(!response.ok) {
-  //
-  //   }
-  // }
+
+  errorHandler: (response) => {
+    if(!response.ok) {
+      const errorMessage = 'Our server is experiencing some difficulties right now, please refresh the page.'
+      throw errorMessage;
+    }
+    return response;
+  },
 
 
   fetchAllCoins: (currency) => {
     return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+    .then(response => fetchRequests.errorHandler(response))
     .then(response => response.json())
   }
 }
